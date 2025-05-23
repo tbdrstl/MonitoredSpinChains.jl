56577d726191a11625ae43193b0a08fbaade2171
Tobias DÃD DÃD¶rstel <tobias.doerstel@gmail.com>
2025-05-23 14:00:08 +0200

    shorter output of total time

--- src/mpiSimulation.jl
+++ src/mpiSimulation.jl
@@ -118,9 +118,7 @@ function simulate(sim::Simulation)
     MPI.Barrier(comm)
     t2 = MPI.Wtime()
     if rank == root
-        println("Simulation took $(t2 - t1) seconds.")
-        println("Simulation took $(t2 - t1)/60 minutes.")
-        println("Simulation took $(t2 - t1)/3600 hours.")
+        println("Simulation took $(round(((t2 - t1)/3600)))h $(round((t2 - t1)/60))min $(round((t2 - t1)))s.")
     end
     MPI.Barrier(comm)
     MPI.Finalize()
